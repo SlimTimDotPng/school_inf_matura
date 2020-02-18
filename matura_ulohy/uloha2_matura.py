ea094740d8af06a93ff7ec6d5efb446bde204d22
SlimTimDotPng <timotejsobota@gmail.com>
2020-02-18 21:22:41 +0100

    dddd

---
+++ matura_ulohy/uloha2_matura.py
@@ -0,0 +1,11 @@
+subor1 = open('D:\\plocha\\python\\vzor.txt','r')
+
+r = subor1.read()
+
+rr = subor1.split()
+
+z = []
+
+print(rr)
+
+subor1.close()
---
+++ python/65_zápis_do_súboru.py
@@ -0,0 +1,4 @@
+subor1 = open('c:\\users\\maturita\\zoznam.txt','w')#vznikne nový priečinok
+subor1.write('Ahoj')
+subor1.write('maturanti')
+subor1.close()
