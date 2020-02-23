d87da8ccef6070e57be59746b50d41d3f5935925
SlimTimDotPng <timotejsobota@gmail.com>
2020-02-23 15:58:43 +0100

    dntn
    her

--- ulohyPL/u07.py
+++ ulohyPL/u07.py
@@ -1,3 +1,18 @@
 # === Úloha 7===
 # Napíšte program, ktorý vymení v postupnosti náhodne vygenerovaných reálnych čísel najmenšie a najväčšie číslo.
 
+from random import randint
+
+l = [ randint(0, 5) for i in range(5) ]
+l_max = max(l)
+l_min = min(l)
+
+print(f"predtym: {l}")
+
+for i in range(len(l)):
+  if l[i] == l_max:
+    l[i] = l_min
+  elif l[i] == l_min:
+    l[i] = l_max
+
+print(f"potom: {l}")
